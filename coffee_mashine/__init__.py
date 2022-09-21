b21a1505983e260f8914825a550f241551b6ddf3
Guavka <guavka@gmail.com>
2022-09-21 16:18:22 +0300

    add coffee machine

---
+++ coffee_mashine/__init__.py
@@ -0,0 +1,8 @@
+from coffee import coffee_machine
+
+if __name__ == '__main__':
+    result = coffee_machine()
+    if coffee_machine():
+        print('User get drink')
+    else:
+        print('User left')
