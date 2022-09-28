4b0e84d47cadf02ed1a19d5216cee55677b51baa
Guavka <guavka@gmail.com>
2022-09-28 14:18:41 +0300

    replace files

--- coffee_mashine/__init__.py
+++
@@ -1,8 +0,0 @@
-from coffee import coffee_machine
-
-if __name__ == '__main__':
-    result = coffee_machine()
-    if coffee_machine():
-        print('User get drink')
-    else:
-        print('User left')
