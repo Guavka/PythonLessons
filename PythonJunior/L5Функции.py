e9fb9cdc6bc541e221bdc81d3bef4b86f1fa1551
Guavka <iluminat94@gmail.com>
2023-01-28 10:26:05 +0300

    Update L5Функции.py

--- PythonJunior/L5Функции.py
+++ PythonJunior/L5Функции.py
@@ -53,7 +53,12 @@ def sqr(number):
 
 result = sqr(15)
 print(result)
-result = (lambda number: number**2)(15)
+
+
+def sqr2(number): return number**2
+
+
+result = sqr2(15)
 print(result)
 
 
@@ -63,7 +68,12 @@ def power(number, power):
 
 result = power(15, 2)
 print(result)
-result = (lambda number, power: number**power)(15,2)
+
+
+def power2(number, power): return number**power
+
+
+result = power2(15, 2)
 print(result)
 
 result = sqr(15)
@@ -75,7 +85,7 @@ print(result)
 # Добавляем аннотации
 
 
-def get_range_sum(left_edge: int, right_edge: int):
+def get_range_sum(left_edge: int, right_edge: int) -> int:
     result = 0
     for number in range(left_edge, right_edge + 1):
         result += number
@@ -84,13 +94,14 @@ def get_range_sum(left_edge: int, right_edge: int):
 # Добавляем описание
 
 
-def print_range_sum(left_edge: int, right_edge: int):
+def print_range_sum(left_edge: int, right_edge: int)->None:
     """
     Функция, которая считает и выводит в консоль сумму чисел в диапазоне
-    Args:
+     Args:
         left_edge (int): Левая граница
         right_edge (int): Правая граница
     """
+    
     result = 0
     for number in range(left_edge, right_edge + 1):
         result += number
@@ -116,3 +127,42 @@ print_range_sum(2, 4)
 
 result = get_range_sum(2, 4) + get_range_sum(1, 8)
 print(result)
+
+'''
+args и kwargs
+
+args - набор из параметров, которые переданы по порядку
+kwargs - набор из параметров, которые переданы по контексту
+'''
+
+
+def get_division(number1: int, number2: int) -> float:
+    return number1/number2
+
+
+result = get_division(10, 2)  # args
+print(result)
+result = get_division(number2=2, number1=10)  # kwargs
+print(result)
+
+
+def print_name(name: str, last_name: str, nick='User123', is_active=True):
+    print(f'Name: {name}\n\
+Last name: {last_name}\n\
+Nick: {nick}\n\
+Is active: {is_active}')
+
+
+# Только args - все по порядку, все нужны
+print_name('Alex', 'Smirnov')
+print_name('Alex', 'Smirnov', 'Guavka')
+print_name('Alex', 'Smirnov', 'User123', False)
+
+# Только kwargs - name, last_name - обязательные, is_active - необязательный
+print_name(name='Alex',
+           last_name='Smirnov',
+           is_active=False)
+
+
+# Гибрид - и то, и то
+print_name('Alex', 'Smirnov', is_active=False)
